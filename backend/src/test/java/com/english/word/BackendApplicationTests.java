889a6d463b0304ed61f241e51fe7b8d424abb5f3
ysys <emoney96@naver.com>
2022-09-19 23:03:06 +0900

    init

---
+++ backend/src/test/java/com/english/word/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.english.word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
